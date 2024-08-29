c136a5ebd5420e996b5af7f67d661936d69b3be2
Arjunppp <arjunmohanak@gmail.com>
2024-08-29 17:26:51 +0530

    dummy structure of backend created

--- chat-app-client/src/components/Input.jsx
+++ chat-app-client/src/components/Input.jsx
@@ -29,9 +29,9 @@ export const Input = (props) => {
             className="absolute right-3 top-1/2 transform -translate-y-1/2"
           >
             {showPassword ? (
-              <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+              <EyeSlashIcon className="h-5 w-5 text-orange-500" />
             ) : (
-              <EyeIcon className="h-5 w-5 text-gray-500" />
+              <EyeIcon className="h-5 w-5 text-orange-500" />
             )}
           </button>
         )}
